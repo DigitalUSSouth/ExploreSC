490f971f86d251f380c10c114ee3f06cbef2da6d
adrq <adrq22@yahoo.com>
2017-11-15 14:22:54 -0500

    fixed api to accept url param

--- exploresc-server/api/views.py
+++ exploresc-server/api/views.py
@@ -2,7 +2,13 @@
 
 # Create your views here.
 from django.http import HttpResponse
+import urllib as ul
+
 
 def index(request):
-    return HttpResponse("api index")
+    query = request.GET['q']
+    with ul.request.urlopen('https://www.digitalussouth.org/api?q='+query+'&start=0') as response:
+        html = response.read()
+    response = html
+    return HttpResponse(response)
 
