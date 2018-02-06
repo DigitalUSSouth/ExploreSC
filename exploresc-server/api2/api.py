4c3d4730d165e020a97c4f5ab4002cc862dec892
adrq <adrq22@yahoo.com>
2018-02-06 16:24:05 -0500

    added spatial search api endpoint

--- exploresc-server/api2/api.py
+++ exploresc-server/api2/api.py
@@ -1,6 +1,7 @@
-from flask import Flask, Response
+from flask import Flask, Response,request
 from pprint import pprint
 import json
+from urllib import request as Req, parse
 app = Flask(__name__)
 
 @app.route('/getAllSCEEntries')
@@ -18,5 +19,18 @@ def get_all_sce_images():
         posts = json.load(json_file)
     json_text = json.dumps(posts['image'],ensure_ascii=False,indent=4, sort_keys=True)
     response = Response(json_text)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+@app.route('/getItemsNear', methods=['GET', 'POST'])
+def get_items_near():
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    latlng = str(lat)+","+str(lng)
+    r =  Req.Request("http://localhost:8983/solr/duss-indexing/select?q=*%3A*{!geofilt}&sfield=geolocation_machine&pt="+latlng+"&d=50&sort=geodist()+asc&wt=json&indent=true")
+    resp = Req.urlopen(r)
+    pprint(vars(resp))
+    #json_text = json.dumps(resp.read(),ensure_ascii=False,indent=4, sort_keys=True)
+    response = Response(resp.read())
     response.headers['Content-Type'] = 'application/json'
     return response
