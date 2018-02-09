a4688de7ceb6650b4ad13f44584229aa39237da5
adrq <adrq22@yahoo.com>
2018-02-09 17:12:33 -0500

    some updates to tileserver docker

---
+++ exploresc-server/tileserver/postgresql/getshapefiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/renderaccount/src/openstreetmap-carto/
+
+scripts/get-shapefiles.py
--- exploresc-server/tileserver/postgresql/initdb.sql
+++ exploresc-server/tileserver/postgresql/initdb.sql
@@ -8,5 +8,9 @@ ALTER TABLE geometry_columns OWNER TO renderaccount;
 
 ALTER TABLE spatial_ref_sys OWNER TO renderaccount;
 
+CREATE ROLE root superuser;
+
+ALTER ROLE root WITH LOGIN;
+
 \q
 
