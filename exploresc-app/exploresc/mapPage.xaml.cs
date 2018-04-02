abcb4dfd90a70dc772e936bc875718fdfe79621d
adrq <adrq22@yahoo.com>
2018-04-02 16:29:34 -0400

    added user location on ios on startup

--- exploresc-app/exploresc/mapPage.xaml.cs
+++ exploresc-app/exploresc/mapPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Plugin.Geolocator;
 
 namespace exploresc
 {
@@ -13,13 +14,26 @@ namespace exploresc
             InitializeComponent();
             var web_view = mainWebView;
 
+
+            //locator.PositionChanged += (sender, e) => {
+                //var position = e.Position;
+                //web_view.Eval(string.Format("updateMap({0}, {1})", position.Latitude, position.Longitude));
+                //latitudeLabel.Text = position.Latitude;
+                //longitudeLabel.Text = position.Longitude;
+            //};
+
+            //Debug.WriteLine("Position Status: {0}", position.Timestamp);
+            //Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+            //Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+
+
             var source = new UrlWebViewSource
             {
                 Url = DependencyService.Get<IBaseUrl>().Get() + "/index.html"
                 // = "https://www.digitalussouth.org/"
             };
             web_view.Source = source;
-            web_view.Navigating += (s, e) =>
+            web_view.Navigating += async (s, e) =>
             {
                 if (e.Url.StartsWith("https://www.google.com"))
                 {
@@ -27,6 +41,22 @@ namespace exploresc
                     {
                         var uri = new Uri(e.Url);
                         Device.OpenUri(uri);
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                    e.Cancel = true;
+                }
+                if (e.Url.EndsWith("getLoc"))
+                {
+                    try
+                    {
+                        var locator = CrossGeolocator.Current;
+
+                        var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(2));
+
+                        web_view.Eval(string.Format("updateMap({0}, {1})", position.Latitude, position.Longitude));
                     }
                     catch (Exception)
                     {
