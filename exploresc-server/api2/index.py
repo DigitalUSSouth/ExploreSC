0e16b55df0354794c87ecc192f3d21451780420e
adrq <adrq22@yahoo.com>
2018-04-23 13:09:32 -0400

    added searching function

--- exploresc-server/api2/index.py
+++ exploresc-server/api2/index.py
@@ -3,13 +3,17 @@ import math
 from nlputils import tokenize_text
 from itertools import repeat
 import numpy as np
+from scipy import spatial
 import json
 from tqdm import tqdm
 from pprint import pprint
-terms_list = {}
+from operator import itemgetter
+import time
 num_docs = 0
 
 def build_index(corpus):
+  terms_list = {}
+  start_time = time.time()
   doc_vectors = []
   pbar = tqdm(total=len(corpus)*2)
   for key,item in corpus.items():
@@ -31,7 +35,7 @@ def build_index(corpus):
   counter = 0
   for doc in doc_vectors:
     for term,tf in doc[0].items():
-      idf = get_idf(term)
+      idf = get_idf(term,terms_list)
       #doc_tfidf = np.zeros([num_terms],dtype=np.float32)
       #doc_tfidf[counter,terms_list[term][1]] = tf*idf
       tfidf_array[counter,terms_list[term][1]] = tf*idf
@@ -39,18 +43,19 @@ def build_index(corpus):
     pbar.update(1)
   pbar.close()
   pprint(len(tfidf_array[0]))
-  return tfidf_array
+  print("Time:",time.time()-start_time,"s")
+  return tfidf_array,terms_list
 
-def add_doc_to_index(doc):
+"""def add_doc_to_index(doc):
   tokens,doc_len = tokenize_text(doc)
   for term in tokens.keys():
     if term in terms_list:
       continue
     else:
       terms_list.append(term)
-  doc_vector = dict(zip(tokens,list(map(get_tf,tokens,repeat(doc_len)))))
+  doc_vector = dict(zip(tokens,list(map(get_tf,tokens,repeat(doc_len)))))"""
 
-def get_idf(term):
+def get_idf(term,terms_list):
   if not term in terms_list:
     return 0
   return math.log(float(1 + num_docs) / 
@@ -59,22 +64,63 @@ def get_idf(term):
 def get_norm_tf(term_freq,doc_len):
   return term_freq/doc_len
 
+def cosine_similarity(a,b):
+  return 1 - spatial.distance.cosine(a, b)
+
+def get_query_tfidf(query_text,terms_list):
+  doc_tfidf = {}
+  tokens,doc_len = tokenize_text(query_text)
+  for token,freq in tokens.items():
+    if token in terms_list:
+      term_index = terms_list[token][1] #previously defined token
+      tf = get_norm_tf(freq,doc_len)
+      idf = get_idf(token,terms_list)
+      doc_tfidf[token] = tf*idf
+  doc_vector = np.zeros([len(terms_list)],dtype=np.float32)
+  for term,tfidf in doc_tfidf.items():
+    term_index = terms_list[term]
+    doc_vector[term_index] = tfidf
+  return doc_vector
+
+def get_rankings(index,terms_list,query):
+  rankings = []
+  query_vector = get_query_tfidf(query,terms_list)
+  for i in range(index.shape[0]):
+    vector = index[i]
+    cos_sim = cosine_similarity(query_vector,vector)
+    rankings(i,cos_sim)
+  rankings.sort(key=itemgetter(1),reverse=True)
+  return rankings
+
+def search(query):
+  saved_index = np.load("index.npz")
+  index = saved_index['arr_0']
+  terms_list = saved_index['arr_1'].item()
+  doc_keys = saved_index['arr_2'].item()
+  rankings = get_rankings(index,terms_list,query)
+  results = []
+  for key,val in rankings:
+    results.append(doc_keys[key])
+  return results
+
 def main():
   with open("sceposts.json","r") as postsfile:
     posts = json.loads(postsfile.read())
   corpus = {}
   counter = 0
+  doc_keys = {}
   for category,cat_items in posts.items():
     #if not category=='document':
     #  continue
     for item in cat_items:
       corpus[counter] = item['text']
+      doc_keys[counter] = {'excerpt':item['content'][:150],'url':item['url'],'title':item['title']}
       counter += 1
       #print(item)
       #break
-  index = build_index(corpus)
+  index,terms_list = build_index(corpus)
   print (type(index))
-  np.savez_compressed("index.npz",index)
+  np.savez_compressed("index.npz",index,terms_list,doc_keys)
 
 if __name__ == "__main__":
   main()
